2aef373923f4be89253f8c869f484bea17ca7551
Lara Rebeca <108086433+larabekers@users.noreply.github.com>
2023-02-15 06:52:11 -0300

    configurando rotas e estilizando elementos

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "../src/styles/global.css";
+import { GlobalStyle } from "./styles/global";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello</p>
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+:root {
+--background: #F0F2F5; 
+--red: #e52e4d;
+--blue: #5429cc;
+--blue-ligh: #6933ff;
+--text-title: #363f5f;
+--background: #fef2f5;
+--shapo: #ffffff;
+}
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html {
+   @media (max-width: 1080px) {
+    font-size: 93.75%;
+   }
+
+   @media (max-width: 720px) {
+    font-size: 87.5%;
+   }
+}
+
+body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+}
+
+button {
+    cursor: pointer;
+}
+
+[disable] {
+    opacity: 8.6;
+    cursor: not-allowed;
+}
+`
